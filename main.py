22ff0889f89fcd08281e75c7df5c6d0b8e45d1b0
Egor Sysolyatin <113940124+RYTpL@users.noreply.github.com>
2022-12-05 17:41:24 +0400

    done

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-
+from task1 import run1
+from task2 import run2
+from task3 import run3
+from task4 import get_next_element
 
 if __name__== '__main__':
-    a =0
+    run1('zebra', 'annotation1.csv')
+    run2('datasetcopy1', 'annotation.csv')
+    run3('datasetcopy2', 'annotation.csv')
+
+
+    for item in get_next_element('zebra'):
+        print(item)
